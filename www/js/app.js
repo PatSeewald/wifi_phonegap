839e2287d71aae4ee67386e934e579deef8bd31f
seewpatr <Cr203210>
2019-06-05 11:20:31 +0200

    geoloc testen

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,17 @@
 console.log('JS loaded')
+
+
+
+document.addEventListener('deviceready', function () {
+    console.log('Device Ready');
+    $(document).ready(function () {
+        console.log('DOM Ready => Alles Ready...');
+
+        navigator.geolocation.getCurrentPosition(function(coords) {
+            $('<div>').html(`${coords.latitude}/${coords.longitude}`).appendTo('body');
+        },function() {
+            $('<div>').html('Error - No Position').appendTo('body')
+        });
+
+    });
+});
